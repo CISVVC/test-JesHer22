12d3885b7beb2550542062db29d32477d647f5c9
JesHer22 <36181973+JesHer22@users.noreply.github.com>
2018-02-22 18:53:16 -0800

    Update Ch 2 HW.py

--- Ch 2 HW.py
+++ Ch 2 HW.py
@@ -2,21 +2,9 @@ import math
 number1_ = 1
 number2_ = 2
 print(number1_ + number2_)
-number1_ = 1
-number2_ = 2
 print(number1_ - number2_)
-number1_ = 1
-number2_ = 2
 print(number1_ * number2_)
-number1_ = 1
-number2_ = 2
 print(number1_ + number2_ //2)
-number1_ = 1
-number2_ = 2
 print(math.fabs(number1_ - number2_))
-number1_ = 1
-number2_ = 2
 print(max(number1_,number2_))
-number1_ = 1
-number2_ = 2
 print(min(number1_,number2_))
