fd021afac3e7bb5ca94bbc187129c40de48cf8d2
JesHer22 <36181973+JesHer22@users.noreply.github.com>
2018-03-02 14:25:26 -0800

    Update Ch 2 HW.py

--- Ch 2 HW.py
+++ Ch 2 HW.py
@@ -1,9 +1,11 @@
 '''
 Devoloper: Jesus Herrera
+
 File Name: main.py
 
-
-Description: short description of what this script does.
+Description:Write the expressions that will use 2 integers and then prints The sum, The difference, The product, The average,
+The distance (absolute value of the difference), The maximum (the larger of the two), 
+The minimum (The smaller of the two)
 
 email address: herreraj112611@student.vvc.edu
 '''
